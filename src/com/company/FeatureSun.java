f55293403f24d89b730e7476c7d3f8e07981246b
sadettin uysal <tubasariz@gmail.com>
2022-05-17 19:30:38 -0400

    Feature Sun added Successfully

---
+++ src/com/company/FeatureSun.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class FeatureSun {
+
+    String heat;
+    String radius;
+    String  color;
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,11 @@ public class Main {
         FactorX.Moon="4";
         FactorX.Planet="2";
         FactorX.Star="10000";
+
+        //adding features to our sun
+        FeatureSun Xfact = new FeatureSun();
+        Xfact.color="green";
+        Xfact.radius="1000000000km";
+        Xfact.heat="309324934898kj";
     }
 }
