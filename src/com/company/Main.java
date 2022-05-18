bae13e1a7664337c24310bcbe501da61a63fd554
sadettin uysal <tubasariz@gmail.com>
2022-05-17 20:12:47 -0400

    feature star added but with some error that needs to be resolved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,5 +14,11 @@ public class Main {
         Xfact.color="green";
         Xfact.radius="1000000000km";
         Xfact.heat="309324934898kj";
+
+        //adding the features for star in the solar system
+        FeatureStars FacX = new FeatureStars();
+        FacX.color=green;
+        FacX.radius="738y4932749km";
+        FacX.size="9999999999m";
     }
 }
